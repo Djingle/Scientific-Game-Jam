373ea25fd0713ac614a98d1a37886fe7a981ba15
Bapt890 <bapt890@gmail.com>
2025-03-08 13:52:35 +0100

    Gnomes can speak !

--- script/gnome.gd
+++ script/gnome.gd
@@ -2,21 +2,41 @@ extends CharacterBody2D
 
 class_name Gnome
 
+@export var speech : String = ""
 var dragging : bool = false
+var mouse_pos : Vector2 = Vector2.ZERO
 
 signal hover(gnome : Gnome)
 signal deselect(gnome : Gnome)
+signal talk(speech : String)
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_released("mouse_click"):
-		dragging = false
-		var tween = create_tween()
-		tween.tween_property(self, "scale", Vector2(1, 1), 0.05)
-		z_index = 0
+		if dragging:
+			dragging = false
+			var tween = create_tween()
+			tween.tween_property(self, "scale", Vector2(1, 1), 0.05)
+			z_index = 0
+		elif mouse_pos != Vector2.ZERO:
+			mouse_pos = Vector2.ZERO
+			$Timer.stop()
+			say()
 	if dragging:
 		var tween = create_tween()
 		tween.tween_property(self, "global_position", get_global_mouse_position(), 0.05)
+	if mouse_pos != Vector2.ZERO and mouse_pos.distance_to(get_global_mouse_position()) > 1:
+		$Timer.stop()
+		drag()
+		mouse_pos = Vector2.ZERO
 		
+func click() -> void:
+	$Timer.start()
+	mouse_pos = get_global_mouse_position()
+
+func say() -> void:
+	mouse_pos = Vector2.ZERO
+	emit_signal("talk", speech)
+
 func drag() -> void:
 	var tween = create_tween()
 	tween.set_parallel()
--- script/ui.gd
+++ script/ui.gd
@@ -1,20 +1,52 @@
 extends Control
 
-var is_dialog_displayed : bool = true
+signal tween_done()
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("debug"):
-		is_dialog_displayed = false if is_dialog_displayed == true else true
-		var anchor_array = [0.5, 0.775, 0.5, 0.775, 0]
-		if is_dialog_displayed:
-			anchor_array = [0.025, 0.65, 0.95, 0.9, 1]
+var is_writing : bool = false
+var is_displayed : bool = false
+
+func _ready() -> void:
+	var anchor_array = [0.5, 0.775, 0.5, 0.775, 0]
+	var tween = create_tween()
+	tween.set_parallel()
+	tween.tween_property($DialogText, "anchor_left", anchor_array[0], 0)
+	tween.tween_property($DialogText, "anchor_top", anchor_array[1], 0)
+	tween.tween_property($DialogText, "anchor_right", anchor_array[2], 0)
+	tween.tween_property($DialogText, "anchor_bottom", anchor_array[3], 0)
+	$DialogText.color.a = 0
+
+func display_manager(mode : bool = true):
+	#Shrinks or displays the text box
+	var anchor_array = [0.5, 0.775, 0.5, 0.775, 0]
+	if mode == true:
+		anchor_array = [0.025, 0.65, 0.95, 0.9, 1]
+	var tween = create_tween()
+	tween.set_parallel()
+	tween.tween_property($DialogText, "anchor_left", anchor_array[0], 0.75)
+	tween.tween_property($DialogText, "anchor_top", anchor_array[1], 0.75)
+	tween.tween_property($DialogText, "anchor_right", anchor_array[2], 0.75)
+	tween.tween_property($DialogText, "anchor_bottom", anchor_array[3], 0.75)
+	tween.tween_property($DialogText, "color:a", anchor_array[4], 0.7)
+	await tween.finished
+	emit_signal("tween_done")
+	is_displayed = mode
+
+func display_text(speech : String):
+	if !is_writing:
+		is_writing = true
+		if !is_displayed:
+			display_manager()
+			await self.tween_done
+		$DialogText/DialogLabel.text = speech
+		$DialogText/DialogLabel.visible_ratio = 0
 		var tween = create_tween()
-		tween.set_parallel()
-		tween.tween_property($DialogText, "anchor_left", anchor_array[0], 0.75)
-		tween.tween_property($DialogText, "anchor_top", anchor_array[1], 0.75)
-		tween.tween_property($DialogText, "anchor_right", anchor_array[2], 0.75)
-		tween.tween_property($DialogText, "anchor_bottom", anchor_array[3], 0.75)
-		tween.tween_property($DialogText, "color:a", anchor_array[4], 0.7)
+		tween.tween_property($DialogText/DialogLabel, "visible_ratio", 1, 0.1 * speech.length())
+		await tween.finished
+		is_writing = false
 	
 func _on_menu_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_dialog_close_button_pressed() -> void:
+	display_manager(false)
+	print("shrink")
